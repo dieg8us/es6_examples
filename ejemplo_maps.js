ac8fd1b5a2dbdfd5286f770e21ac4b8f92ef8db0
Diegous <duribe@cobralex.cl>
2017-10-11 17:53:32 -0300

    ejemplo maps

---
+++ ejemplo_maps.js
@@ -0,0 +1,50 @@
+
+
+let mapa = new Map();
+
+mapa.set( "nombre", "Diego" );
+mapa.set( "edad", 31 );
+mapa.set( "apellido" );
+
+console.log( mapa );
+console.log( mapa.size );
+
+console.log( mapa.get( "nombre" ) );
+console.log( mapa.get( "edad" ) );
+console.log( mapa.has( "nombre") );
+console.log( mapa.has( "apellido") );
+
+mapa.delete( "nombre" );
+console.log( mapa.has( "nombre") );
+console.log( mapa.get( "nombre") );
+
+mapa.set( "edad" );
+console.log( mapa );
+
+mapa.clear();
+
+console.log( mapa );
+
+console.log( "===============================" );
+
+// inicializacion mapa
+
+let mapa2 = new Map( [ ["nombre", "Diego"], [null, 123456] ] );
+
+console.log( mapa2 );
+
+console.log( mapa.get(null) );
+
+// for Each Maps
+
+let mapa3 = new Map( [ ["nombre", "Diego"], ["edad", 31] ] );
+
+mapa3.forEach( function( value, llave, mapaOriginal ) {
+  console.log( `Llave: ${llave}, valor: ${value}`);
+  console.log( mapaOriginal );
+});
+
+mapa3.forEach( (valor, llave, mapaOriginal) => {
+  console.log(`Llave: ${llave}, valor: ${valor}`);
+  console.log( mapaOriginal );
+});
--- ejemplo_sets.js
+++ ejemplo_sets.js
@@ -39,3 +39,29 @@ console.log( items4 );
 items4.clear();
 
 console.log( items4 );
+
+console.log( "===============================" );
+
+let personas = new Set(["Diego", "Maria", "Susana"]);
+
+personas.forEach( function( valor, llave, setOriginal ) {
+  console.log( valor, llave, setOriginal );
+  console.log( personas === setOriginal );
+});
+
+console.log( "=================================" );
+
+let numeros = [1,2,3,4,5,6,7,7,7,7,7,7,7,8,8,8,8,8,1,2,3,4,5];
+let setNumeros = new Set( numeros );
+
+console.log( setNumeros );
+
+let arrayNumeros = eliminaDuplicados( numeros );
+
+function eliminaDuplicados( items ) {
+  return [... new Set(items)];
+}
+
+console.log( "=================================" );
+
+// weekset similar a los set
